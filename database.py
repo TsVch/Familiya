7bfec4aac5b850f0f692872d43490bc996b1bf6d
TsVch <vyacheslav.czoj@bk.ru>
2026-04-23 11:49:34 +0300

    MVP v1.0.2 Add PostgreDB for stable version

--- database.py
+++ database.py
@@ -1,15 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./family_chat.db"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./family_chat.db"
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
